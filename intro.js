96baae4232909e1d6c4c7a2953673bfc4cbfa1ac
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-05-09 22:29:40 +0300

    feat: declared variables

---
+++ intro.js
@@ -0,0 +1,30 @@
+let myCat = "zaytoon";
+myCat = "bobi";
+let myAge = 24;
+
+let dolphinGoodbye = "So long and thanks for all the fish";
+let iAmAlive = true;
+let test = 11 > 3;
+
+let myNameList = ["milky", "bopti", "shilem"];
+let myAgeList = [20, 12, 35];
+myNameArray[0]; // should return 'milky'
+myNumberArray[2]; // should return 40
+
+let student = {name: "milky" , age: 20 , occupation: "a collage student" };  //object
+ student.name; //to retrive information from the object.
+
+ let myNumber = "500"; 
+typeof myNumber; // returns a string
+myNumber = 500; 
+typeof myNumber; //returns a number data type
+
+const count = 1; // declare and intialize at the same line
+
+const bird = { species: "Kestrel" };
+console.log(bird.species); // "Kestrel"
+
+bird.species = "Striated Caracara";
+console.log(bird.species); // "Striated Caracara"
+
+
