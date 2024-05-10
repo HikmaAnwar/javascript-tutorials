8f60db7f3c4f8f7c4aee5ba9928165fddb39f3a5
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-05-10 14:59:56 +0300

    minor change

--- intro.js
+++ intro.js
@@ -41,3 +41,9 @@ let x = "John" + " " + "Doe"; // concatenation of string
 let $ = "Hello World";
 let $$$ = 2;
 let $myMoney = 5;
+
+
+let x = 250;
+    let y = 40.5;
+    console.log("Value of x=" + x);
+    console.log("Value of y=" + y);
