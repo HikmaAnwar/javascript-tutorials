448df9663737bf5242776fc643d8d9b732c87702
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-05-10 20:15:22 +0300

    fix: minor change

--- intro.js
+++ intro.js
@@ -78,3 +78,23 @@ let a = new Array();
     console.log("value of a=" + a);
     console.log("value of b" + b);
     console.log("value of d=" + d);
+
+    if (choice === "sunny" && temperature < 86) {
+        para.textContent = `It is ${temperature} degrees outside — nice and sunny. Let's go out to the beach, or the park, and get an ice cream.`;
+      } else if (choice === "sunny" && temperature >= 86) {
+        para.textContent = `It is ${temperature} degrees outside — REALLY HOT! If you want to go outside, make sure to put some sunscreen on.`;
+      }
+
+      if (iceCreamVanOutside || houseStatus === "on fire") {
+        console.log("You should leave the house quickly.");
+      } else {
+        console.log("Probably should just stay in then.");
+      }
+
+      if (!(iceCreamVanOutside || houseStatus === "on fire")) {
+        console.log("Probably should just stay in then.");
+      } else {
+        console.log("You should leave the house quickly.");
+      }
+
+      
