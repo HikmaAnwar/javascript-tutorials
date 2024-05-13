a57499a2de3d38d37853ccdbae8a17a1bfd72953
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-05-13 18:24:46 +0300

    fix: add another loop for calculating sum

--- looping.js
+++ looping.js
@@ -1,4 +1,12 @@
 // for loop example 
 for (let i = 1; i < 6; i++) {
-    console.log(i);
+     console.log(i);
 }
+//  ex 2 Display Sum of n Natural Numbers
+let sum = 0;
+const n = 100;
+
+ for(let i = 0; i <= n; i++){
+    sum += i;
+ }
+  console.log('sum:' + sum);
