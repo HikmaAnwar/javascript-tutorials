ecb0aaabebee57c5e520b890a79243dfa527db52
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-05-13 18:29:28 +0300

    fix: minor change

--- looping.js
+++ looping.js
@@ -10,3 +10,10 @@ const n = 100;
     sum += i;
  }
   console.log('sum:' + sum);
+//  
+
+const fruits = ["apple", "banana", "cherry"];
+
+for(let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
