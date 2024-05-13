1c22f8b3fe5148cf2464f3cc280c0cb690cb7eb7
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-05-13 19:11:45 +0300

    fix: added a function to add two numbers

--- functions.js
+++ functions.js
@@ -42,3 +42,15 @@ greet1("donny");
 // output 
 // hello bopti
 // hello donny
+
+// function with two arguments
+function addNumbers(num1, num2) {
+    let sum = num1 + num2;
+   console.log(`Sum: ${sum}`);
+}
+
+// call function by passing two arguments
+addNumbers(5, 4);
+
+// Output:
+// Sum: 9
