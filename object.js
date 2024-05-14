df266f584cc942d823fb5a0d8d01e54612304748
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-05-14 21:17:10 +0300

    feat: added another function

--- object.js
+++ object.js
@@ -10,3 +10,17 @@ console.log(myCar["model"])
 
 myCar["do i like?"] = "i hate my car.";
 console.log(myCar["do i like?"]);
+
+function showProps(obj, objName) {
+    var result ="";
+    for(var i in obj){
+        // obj.hasOwnProperty() is used to filter
+        // out properties from the objects protyoe chain
+        if(obj.hasOwnProperty(i)) {
+            result += objName + "." + i + "=" + obj[i] + "\n";
+
+        }
+    }
+    return result;
+}
+console.log(showProps(myCar, "myCar"));
