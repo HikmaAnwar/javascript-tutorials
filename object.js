2433026d878f6003e94a1a09d8f5559115509578
HikmaAnwar <Hikmaanwar89@gmail.com>
2024-05-18 14:27:25 +0300

    feat: added some code for accessing and manuplating nested objects

--- object.js
+++ object.js
@@ -89,4 +89,21 @@ console.log(dishes);
 console.log(dishes.hasOwnProperty('plates'));
 console.log(dishes.hasOwnProperty('cups'));
 
-// 
+// accesssing and modifying nested objects
+
+var ourStorage ={
+    "desk": {
+        "drawer": "stapler"
+    },
+    "cabinet": {
+        "top drawer": {
+            "folder1": "a file",
+            "folder2": "secrets"
+        },
+        "bottom drawer": "soda"
+    }
+};
+console.log(ourStorage.cabinet["top drawer"].folder2);
+console.log(ourStorage.desk.drawer);
+ourStorage.cabinet["top drawer"].folder2 = "cake recipe";
+console.log(ourStorage.cabinet["top drawer"].folder2);
